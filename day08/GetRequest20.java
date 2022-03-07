cf96ca9414862b3bf3d949a804d8c9d1a05e0cc2
meryem catalbas <meryemcatalbas54@gmail.com>
2022-03-08 00:24:07 +0300

    third

--- day08/GetRequest20.java
+++ day08/GetRequest20.java
@@ -1,13 +1,18 @@
-package get_http_request.day08;
+package day_08;
 
-import base_url.JsonPlaceHolderBaseUrl;
+import Base_Url.JsonPlaceHolderBaseUrl;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
+import java.util.HashMap;
 
-public class GetRequest20 extends JsonPlaceHolderBaseUrl {
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class C20_GetRequest extends JsonPlaceHolderBaseUrl {
      /*
 https://jsonplaceholder.typicode.com/todos/2
 1) Status kodunun 200,
@@ -20,27 +25,56 @@ https://jsonplaceholder.typicode.com/todos/2
          "Server" değerinin "cloudflare" olduğunu test edin…
 */
 
-@Test
-    public void test(){
+    @Test
+    public void test20(){
+        //1-URL OLUSTUR
+        spec04.pathParams("parametre1", "todos", "parametre2", 2);
 
-    //https://jsonplaceholder.typicode.com/todos/2
-    Response response = given().spec(spec04).when().get("/{1}/{2}/{3}");
-    //"/{1}/{2}/{3}" -> /api/v1/employees
-    //http://dummy.restapiexample.com/api/v1/employees
+        //2-ExpectedData olustur
+        HashMap<String, Object> expectedData= new HashMap<>();
+        expectedData.put("statusCode", 200);
+        expectedData.put("completed", false);
+        expectedData.put("title", "quis ut nam facilis et officia qui");
+        expectedData.put("userId", 1);
+        expectedData.put("via", "1.1 vegur");
+        expectedData.put("Server", "cloudflare");
 
-    //response.prettyPrint();
+        //3-REQUEST ve RESPONSE
+        Response response= given().spec(spec04).when().get("/{parametre1}/{parametre2}");
+        response.prettyPrint();
 
-    //1) Status kodunun 200,
-    Assert.assertEquals(200, response.statusCode());
-    response.then().assertThat().statusCode(200);
-
-}
-
-
-
-
-
-
+        //4-DOGRULAMA
+        //I.YOL>matchers class
+        response.then().assertThat()
+                .statusCode((Integer)expectedData.get("statusCode"))
+                .headers("via",equalTo(expectedData.get("via"))
+                        ,"server",equalTo(expectedData.get("server")))
+                .body("completed",equalTo(expectedData.get("completed"))
+                        ,"title",equalTo(expectedData.get("title")),
+                        "userId", equalTo(expectedData.get("userId")));
 
 
+        //II.YOL>>JsonPath
+        JsonPath json=response.jsonPath();
+        //Assert.assertEquals(200, json.getInt("statusCode"));
+        //Assert.assertEquals(false, json.getString("completed"));
+        //Assert.assertEquals("quis ut nam facilis et officia qui", json.getString("title"));
+        //Assert.assertEquals(1, json.getInt("userId"));
+        //Assert.assertEquals("1.1 vegur", json.getString("via"));
+        //Assert.assertEquals("cloudflare", json.getString("Server"));
+        Assert.assertEquals(expectedData.get("statusCode"),response.statusCode());
+        Assert.assertEquals(expectedData.get("via"), response.getHeader("via"));
+        Assert.assertEquals(expectedData.get("Server"),response.getHeader("Server"));
+        Assert.assertEquals(expectedData.get("completed"), json.getBoolean("completed"));
+        Assert.assertEquals(expectedData.get("title"), json.getString("title"));
+        Assert.assertEquals(expectedData.get("userId"), json.getInt("userId"));
+
+        //III.YOL>>>De-serialization
+        HashMap<String, Object> actualData = response.as(HashMap.class);
+        System.out.println(actualData);
+
+        Assert.assertEquals(expectedData.get("userId"), actualData.get("userId"));
+        Assert.assertEquals(expectedData.get("completed"), actualData.get("completed"));
+        Assert.assertEquals(expectedData.get("title"), actualData.get("title"));
+    }
 }
